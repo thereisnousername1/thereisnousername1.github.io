b7fcf47fd9adbb890ba6ce0366f0de27418bdfb1
thereisnousername1 <126057736+thereisnousername1@users.noreply.github.com>
2026-09-17 03:54:30 +0800

    update project 3 cover with mov

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,7 +22,7 @@ const projects = [
     tags: ['C++', 'SDL2', 'scrolling background', 'smooth music'],
     link: 'https://github.com/thereisnousername1/LabsOpenGLAdvanced/tree/main',
     color: '#1060f0',
-    cover: '/cover/project3cover.jpeg'
+    cover: '/cover/project3cover.mov'
   },
 ]
 
